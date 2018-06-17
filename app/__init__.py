6ecc2b2aa54dfc53b95bb11bae74277bc7bce46d
Andrii Kobryk <a.kobryk@gmail.com>
2018-06-17 17:21:21 +0300

    add a route for media

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from flask_mail import Mail
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from werkzeug import SharedDataMiddleware
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -18,6 +19,11 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+app.add_url_rule('/media/<filename>', 'get_media',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/media':  app.config['UPLOAD_FOLDER']
+})
 
 from .models import User  # noqa
 
