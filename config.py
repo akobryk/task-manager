46427982077aaa5491b598e2c6295f49ab40dbcc
Andrii Kobryk <a.kobryk@gmail.com>
2018-06-17 17:25:06 +0300

    update config

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+import os
 
 DEBUG = False
 TESTING = False
 ENV = 'production'
 SECRET_KEY = b'\xad\x9d\xbf\x93|\xd3\xab\x81\x14Z\xd0\xfb\xd6-\xd4\x84'
 SECURITY_PASSWORD_SALT = 'this_is_password_salt'
+UPLOAD_FOLDER = os.path.join('media')
 
 try:
     from local_config import *  # noqa
